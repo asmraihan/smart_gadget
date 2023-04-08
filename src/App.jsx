24dbcfb88f8a5da23858cdc9c5c33326389cb3da
asmraihan <asmraihanctg14@gmail.com>
2023-04-08 15:56:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>Welcome to SmartGadget</h1>
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: <App></App>
+}])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}></RouterProvider>
+)
